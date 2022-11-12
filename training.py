e4272dac03b3d1a66e71f940e7eae8d08e745f7c
Zanuar <zanuar.er@gmail.com>
2022-11-12 13:30:01 +0700

    add dummy code to train the model

--- training.py
+++ training.py
@@ -8,25 +8,35 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
 
-###################Load config.json and get path variables
+# Load config.json and get path variables
 with open('config.json','r') as f:
     config = json.load(f) 
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
-model_path = os.path.join(config['output_model_path']) 
+model_path = os.path.join(config['output_model_path'])
+data_path = '/finaldata.csv'
 
 
-#################Function for training the model
+# Function for training the model
 def train_model():
-    
-    #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+    data = pd.read_csv(os.getcwd() + '/' + dataset_csv_path + data_path)
+
+    X = data.drop(['exited'], axis=1)
+    y = data['exited']
+
+    # use this logistic regression for training
+    model = LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
                     intercept_scaling=1, l1_ratio=None, max_iter=100,
                     multi_class='warn', n_jobs=None, penalty='l2',
                     random_state=0, solver='liblinear', tol=0.0001, verbose=0,
                     warm_start=False)
     
-    #fit the logistic regression to your data
+    # fit the logistic regression to your data
+    model.fit(X, y)
     
     #write the trained model to your workspace in a file called trainedmodel.pkl
+    pickle.dump(model, open('./production/' + deployedname, 'wb'))
+
+if __name__ == '__main__':
+    train_model()
 
