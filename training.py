1bcf740a960d5514275c62e3b77860290b37e2fe
Zanuar <zanuar.er@gmail.com>
2022-11-15 07:25:42 +0700

    update `training.py`

--- training.py
+++ training.py
@@ -15,27 +15,50 @@ with open('config.json','r') as f:
 dataset_csv_path = os.path.join(config['output_folder_path']) 
 model_path = os.path.join(config['output_model_path'])
 data_path = '/finaldata.csv'
+model_name = 'trainedmodel.pkl'
 
 
 # Function for training the model
 def train_model():
     data = pd.read_csv(os.getcwd() + '/' + dataset_csv_path + data_path)
 
-    X = data.drop(['exited'], axis=1)
+    x = data.drop(['corporation', 'exited'], axis=1)
     y = data['exited']
 
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True)
+
+    print(x_train, y_train)
+
     # use this logistic regression for training
-    model = LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-                    intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
-                    random_state=0, solver='liblinear', tol=0.0001, verbose=0,
-                    warm_start=False)
+    model = LogisticRegression(
+        C=1.0,
+        class_weight=None,
+        dual=False,
+        fit_intercept=True,
+        intercept_scaling=1,
+        l1_ratio=None,
+        max_iter=100,
+        n_jobs=None,
+        penalty='l2',
+        random_state=0,
+        solver='liblinear',
+        tol=0.0001,
+        verbose=0,
+        warm_start=False
+    )
     
     # fit the logistic regression to your data
-    model.fit(X, y)
-    
+    model.fit(x_train, y_train)
+
     #write the trained model to your workspace in a file called trainedmodel.pkl
-    pickle.dump(model, open('./production/' + deployedname, 'wb'))
+    if os.path.isdir(model_path) is False:
+        os.makedirs(model_path)
+        pickle.dump(model, open('./{}/{}'.format(model_path, model_name), 'w'))
+    else:
+        pickle.dump(model, open('./{}/{}'.format(model_path, model_name), 'w'))
+
+
+
 
 if __name__ == '__main__':
     train_model()
