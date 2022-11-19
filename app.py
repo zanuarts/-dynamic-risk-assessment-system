ec162a70017c52472018246cb7972b3cf0cf32d0
Zanuar <zanuar.er@gmail.com>
2022-11-19 09:34:23 +0700

    update steps 4

--- app.py
+++ app.py
@@ -2,15 +2,14 @@ from flask import Flask, session, jsonify, request
 import pandas as pd
 import numpy as np
 import pickle
-import create_prediction_model
-import diagnosis 
-import predict_exited_from_saved_model
+import diagnostics
+import scoring
 import json
 import os
 
 
 
-######################Set up variables for use in our script
+# Set up variables for use in our script
 app = Flask(__name__)
 app.secret_key = '1652d576-484a-49fd-913a-6879acfa6ba4'
 
@@ -22,29 +21,38 @@ dataset_csv_path = os.path.join(config['output_folder_path'])
 prediction_model = None
 
 
-#######################Prediction Endpoint
+# Prediction Endpoint
 @app.route("/prediction", methods=['POST','OPTIONS'])
 def predict():        
-    #call the prediction function you created in Step 3
-    return #add return value for prediction outputs
+    # call the prediction function you created in Step 3
+    result = diagnostics.model_predictions()
+    # add return value for prediction outputs
+    return result
 
-#######################Scoring Endpoint
+# Scoring Endpoint
 @app.route("/scoring", methods=['GET','OPTIONS'])
 def stats():        
-    #check the score of the deployed model
-    return #add return value (a single F1 score number)
+    # check the score of the deployed model
+    score = scoring.score_model()
+    # add return value (a single F1 score number)
+    return score
 
-#######################Summary Statistics Endpoint
+# Summary Statistics Endpoint
 @app.route("/summarystats", methods=['GET','OPTIONS'])
 def stats():        
-    #check means, medians, and modes for each column
-    return #return a list of all calculated summary statistics
+    # check means, medians, and modes for each column
+    data_summary = diagnostics.dataframe_summary()
+    # return a list of all calculated summary statistics
+    return data_summary
 
-#######################Diagnostics Endpoint
+
+# Diagnostics Endpoint
 @app.route("/diagnostics", methods=['GET','OPTIONS'])
 def stats():        
-    #check timing and percent NA values
-    return #add return value for all diagnostics
+    # check timing and percent NA values
+    timing = diagnostics.execution_time()
+    # add return value for all diagnostics
+    return timing
 
 if __name__ == "__main__":    
     app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
--- reporting.py
+++ reporting.py
@@ -7,22 +7,34 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import json
 import os
+import joblib
 
 
 
-###############Load config.json and get path variables
+# Load config.json and get path variables
 with open('config.json','r') as f:
     config = json.load(f) 
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
+test_data_path = os.path.join(config['test_data_path'])
+test_data_name = 'testdata.csv'
+model_path = os.path.join(config['output_model_path'])
+model_name = 'trainedmodel.pkl'
 
 
-
-
-##############Function for reporting
+# Function for reporting
 def score_model():
-    #calculate a confusion matrix using the test data and the deployed model
-    #write the confusion matrix to the workspace
+    # calculate a confusion matrix using the test data and the deployed model
+    test_data = pd.read_csv('./{}/{}'.format(test_data_path, test_data_name))
+
+    x_test = test_data.drop(['corporation', 'exited'], axis=1)
+    y_test = test_data['exited']
+
+    model = joblib.load('./{}/{}'.format(model_path, model_name))
+    y_pred = model.predict(x_test)
+    result = metrics.confusion_matrix(y_test, y_pred)
+    # write the confusion matrix to the workspace
+
 
 
 
